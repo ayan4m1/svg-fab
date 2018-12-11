281a7db4edf7a948893a4afba7165a6ba4baa963
ayan4m1 <andrew@bulletlogic.com>
2018-12-11 15:01:32 -0500

    initial import - add babel, gulp, and simple script to extract path from sketch SVGs

---
+++ gulpfile.babel.js
@@ -0,0 +1,20 @@
+import del from 'del';
+import gulp from 'gulp';
+import babel from 'gulp-babel';
+
+const src = './src/**/*.js';
+const dst = './bin/';
+
+const build = () =>
+  gulp
+    .src(src)
+    .pipe(babel())
+    .pipe(gulp.dest(dst));
+
+const clean = () => del(dst);
+const watch = () => gulp.watch(src, build);
+
+gulp.task('build', build);
+gulp.task('clean', clean);
+gulp.task('watch', gulp.series(build, watch));
+gulp.task('default', gulp.series(clean, build));
---
+++ src/index.js
@@ -0,0 +1,61 @@
+import { readFile } from 'fs';
+import commander from 'commander';
+import { DOMParser } from 'xmldom';
+import { useNamespaces } from 'xpath';
+
+commander
+  .version('0.1.0')
+  .usage('-n <name> <svg>')
+  .option('-n, --name <value>', 'Name to use in construction of <symbol> element')
+  .parse(process.argv);
+
+if (commander.args.length < 1) {
+  // silly that we have to pass an identity transform here
+  commander.outputHelp(text => text);
+  process.exit(1);
+}
+
+// get our arguments
+const svgPath = commander.args.shift();
+const svgName = commander.name;
+
+readFile(svgPath, 'utf-8', (error, data) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  // need to use the SVG namespace when querying the source
+  const doc = new DOMParser().parseFromString(data);
+  const select = useNamespaces({ svg: 'http://www.w3.org/2000/svg' });
+  const rootElement = select('/svg:svg', doc).shift();
+  const pathElement = select('//svg:path', doc).shift();
+
+  if (!rootElement) {
+    console.error(`There is no <svg> element in ${svgPath}`);
+    process.exit(1);
+  }
+
+  if (!pathElement) {
+    console.error(`There is no <path> element in ${svgPath}`);
+    process.exit(1);
+  }
+
+  // pull the data we need from the source SVG
+  const [ width, height, viewBox, points ] = [
+    rootElement.getAttribute('width'),
+    rootElement.getAttribute('height'),
+    rootElement.getAttribute('viewBox'),
+    pathElement.getAttribute('d')
+  ];
+
+  // yes, the indentation is screwy here. leave it be.
+  console.log(`<symbol
+  id="icon-${svgName}"
+  className="symbol-${svgName}"
+  width="${width}"
+  height="${height}"
+  viewBox="${viewBox}"
+    <path d="${points}" />
+</symbol>`);
+});
