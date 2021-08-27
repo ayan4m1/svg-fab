5c3f72cbbd8a5c68729a0507c2870b980d52e02a
ayan4m1 <andrew@bulletlogic.com>
2021-08-27 01:10:52 -0400

    upgrade all deps and add eslint/prettier

--- src/convert.js
+++ src/convert.js
@@ -1,56 +1,51 @@
 #!/usr/bin/env node
-import SVGO from 'svgo';
+import { optimize } from 'svgo';
 import { readFile } from 'fs';
 import slugify from 'slugify';
 import { basename } from 'path';
-import commander from 'commander';
+import { Command } from 'commander';
 
 import { getDocument, getDimensions, getPathData } from './xml';
 
-commander
-  .usage('-n [iconName] <file ...>')
-  .version('0.2.0', '-v, --version')
-  .option('-n, --name [iconName]', 'descriptor of the icon')
+const program = new Command();
+
+program
+  .version('0.2.1')
+  .argument('<files...>')
+  .option('--icon-name, --name, -n', 'icon name for generated XML')
   .parse(process.argv);
 
-const { args } = commander;
-
-// show help if required argument is not present
-if (args.length < 1) {
-  commander.outputHelp();
-  process.exit(1);
-}
-
-// prepare SVGO and get our parsed arguments
-const optimizer = new SVGO({
-  plugins: [
-    {
-      convertPathData: {
-        floatPrecision: 2,
-        leadingZero: false,
-        forceAbsolutePath: true
-      }
-    },
-    {
-      removeViewBox: false
-    }
-  ]
-});
+const { args } = program;
 
 for (const svgPath of args) {
   const { iconName } = args;
   const svgName = slugify(iconName ? iconName : basename(svgPath, '.svg'));
 
-  readFile(svgPath, 'utf-8', async (error, data) => {
+  readFile(svgPath, 'utf-8', (error, data) => {
     if (error) {
-      console.error(error);
-      process.exit(1);
+      throw error;
     }
 
     // extract the SVG file into a string
     const doc = getDocument(data);
     // apply optimizations
-    const optimized = await optimizer.optimize(doc);
+    const optimized = optimize(doc, {
+      plugins: [
+        {
+          name: 'preset-default',
+          params: {
+            overrides: {
+              convertPathData: {
+                floatPrecision: 2,
+                leadingZero: false,
+                forceAbsolutePath: true
+              },
+              removeViewBox: false
+            }
+          }
+        }
+      ]
+    });
     // extract the SVG from the optimized SVG
     const optimizedDoc = getDocument(optimized.data);
     // extract dimension metadata from SVG
@@ -59,6 +54,7 @@ for (const svgPath of args) {
     const points = getPathData(optimizedDoc);
 
     // spit out a blob of HTML for a sprite sheet
+    // eslint-disable-next-line no-console
     console.log(`
 <symbol
   id="icon-${svgName}"
