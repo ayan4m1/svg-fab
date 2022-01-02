e28db8940777b8d405d8f8dbd7125d34904193b1
ayan4m1 <andrew@bulletlogic.com>
2022-01-01 20:10:09 -0500

    switch from gulp to rollup

---
+++ rollup.config.js
@@ -0,0 +1,28 @@
+import { babel } from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import autoExternal from 'rollup-plugin-auto-external';
+import eslint from '@rollup/plugin-eslint';
+
+export default {
+  input: './src/convert.js',
+  output: {
+    dir: './bin/',
+    format: 'cjs',
+    exports: 'auto'
+  },
+  plugins: [
+    // configuration
+    autoExternal(),
+    // prepare
+    eslint(),
+    // parsers
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**'
+    }),
+    // transformers
+    nodeResolve({
+      preferBuiltins: true
+    })
+  ]
+};
